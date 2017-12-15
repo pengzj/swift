994e78fbbdbba00b3cbf8d66cd26ffb3ffee2099
francis.peng <francis.peng@ringcentral.com>
2017-12-15 13:23:28 +0800

    add bitmap

---
+++ bitmap/bitmap.go
@@ -0,0 +1,44 @@
+package bitmap
+
+const (
+	SHIFT = 5
+	MASK = 0x1F
+)
+type Bitmap struct {
+	db []uint
+}
+
+func NewBitmap() *Bitmap {
+	return &Bitmap{db: make([]uint, 0, 50)}
+}
+
+func (b *Bitmap) Set(i uint) {
+	offset := int(i >>SHIFT + 1) - len(b.db)
+	if offset > 0 {
+		b.db = append(b.db, make([]uint, offset)...)
+	}
+
+	b.db[i>>SHIFT] |= (1 << (i & MASK))
+}
+
+func (b *Bitmap) Clear(i uint)  {
+	offset := int(i >>SHIFT + 1) - len(b.db)
+	if offset > 0 {
+		b.db = append(b.db, make([]uint, offset)...)
+	}
+
+	b.db[i>>SHIFT] &= ^(1<<(i & MASK));
+}
+
+func (b *Bitmap) Get(i uint) uint {
+	offset := int(i >>SHIFT + 1) - len(b.db)
+	if offset > 0 {
+		return 0
+	}
+	return b.db[i>>SHIFT] & (1<<(i&MASK))
+}
+
+func (b *Bitmap) All() []uint {
+	return b.db
+}
+
---
+++ bitmap/bitmap_test.go
@@ -0,0 +1,31 @@
+package bitmap
+
+import "testing"
+
+var (
+	bt *Bitmap = NewBitmap()
+)
+
+func TestBitmap_Get(t *testing.T) {
+	bt.Set(100)
+	bt.Set(88)
+	bt.Set(0)
+	t.Log(bt.All())
+	t.Log("bt.Get(88) = ", bt.Get(88))
+	t.Log("bt.Get(100) = ", bt.Get(0))
+	if bt.Get(88) == 0 {
+		t.Fatal("100 not in bitmap")
+	}
+	//bt.Clear(100)
+	bt.Clear(88)
+	t.Log(bt.All())
+}
+
+
+func BenchmarkBitmap_All(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		bt.Set(100)
+		bt.Set(88)
+		bt.Get(3000)
+	}
+}
